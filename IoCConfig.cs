5149317456ddb98589759559277eea3c33254afa
stanley <stanley@bbdc20ee-fbae-9e43-ab44-8db8742cc3c9>
2020-04-24 08:54:17 +0000

    git-svn-id: https://jb-vsts.jbjob.com.tw:8443/svn/customer/JBHR2/HR_WebApi/HR_WebApi@38598 bbdc20ee-fbae-9e43-ab44-8db8742cc3c9

---
+++ IoCConfig.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace HR_WebApi
+{
+    public class IoCConfig
+    {
+
+        public IoCConfig()
+        {
+
+        }
+        public static IServiceCollection Configure(IConfiguration Configuration, IServiceCollection services)
+        {
+            services.AddSingleton<NLog.ILogger>(NLog.LogManager.GetLogger("HR"));
+            //services.AddScoped<JBHRIS.Api.Service.Employee.Normal.IEmployeeInfoService, JBHRIS.Api.Service.Employee.Normal.EmployeeInfoService>();
+            //services.AddScoped<JBHRIS.Api.Service.Employee.Normal.IEmployeeListService, JBHRIS.Api.Service.Employee.Normal.EmployeeListService>();
+            //services.AddScoped<JBHRIS.Api.Service.Employee.View.IEmployeeViewService, JBHRIS.Api.Service.Employee.View.EmployeeViewService > ();
+
+            //services.AddScoped<JBHRIS.Api.Dal.Employee.Normal.IEmployee_Normail_GetPeopleByBirthday, JBHRIS.Api.Dal.JBHR.Employee.Employee_Normail_GetPeopleByBirthday>();
+            return services;
+        }
+
+
+
+        
+
+
+
+    }
+}
