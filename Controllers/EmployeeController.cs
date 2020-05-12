3b8cd73a972cc60deb81cd7b97ee29da99305549
ming <ming@bbdc20ee-fbae-9e43-ab44-8db8742cc3c9>
2020-05-12 01:16:48 +0000

    git-svn-id: https://jb-vsts.jbjob.com.tw:8443/svn/customer/JBHR2/HR_WebApi/HR_WebApi@38750 bbdc20ee-fbae-9e43-ab44-8db8742cc3c9

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using JBHRIS.Api.Dto.Employee.Normal;
+using JBHRIS.Api.Dto.Employee.View;
 using JBHRIS.Api.Service.Employee.Normal;
+using JBHRIS.Api.Service.Employee.View;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -15,11 +17,13 @@ namespace HR_Api_Demo.Controllers
     {
         private IEmployeeInfoService _employeeService;
         private IEmployeeListService _employeeListService;
+        private IEmployeeViewService _employeeViewService;
 
-        public EmployeeController(IEmployeeInfoService employeeService,IEmployeeListService employeeListService)
+        public EmployeeController(IEmployeeInfoService employeeService, IEmployeeListService employeeListService, IEmployeeViewService employeeViewService)
         {
             _employeeService = employeeService;
             _employeeListService = employeeListService;
+            _employeeViewService = employeeViewService;
         }
         [HttpPost]
         [Route("GetEmployeeInfo")]
@@ -34,10 +38,34 @@ namespace HR_Api_Demo.Controllers
             return _employeeService.GetEmployeeInfoByDept(employeeInfo_GetPeopleByDept_Entry.deptList, Convert.ToDateTime(employeeInfo_GetPeopleByDept_Entry.CheckDate));
         }
         [HttpPost]
+        [Route("GetEmployeeInfoByDeptWithMask")]
+        public List<EmployeeInfoByDeptWithMaskDto> GetEmployeeInfoByDeptWithMask([FromBody]EmployeeInfo_GetPeopleByDept_Entry employeeInfo_GetPeopleByDept_Entry)
+        {
+            return _employeeService.GetEmployeeInfoByDeptWithMask(employeeInfo_GetPeopleByDept_Entry.deptList, Convert.ToDateTime(employeeInfo_GetPeopleByDept_Entry.CheckDate));
+        }
+        [HttpPost]
         [Route("GetPeopleByDept")]
         public List<string> GetPeopleByDept([FromBody]EmployeeInfo_GetPeopleByDept_Entry employeeInfo_GetPeopleByDept_Entry)
         {
             return _employeeListService.GetPeopleByDept(employeeInfo_GetPeopleByDept_Entry.deptList, Convert.ToDateTime(employeeInfo_GetPeopleByDept_Entry.CheckDate));
+        }
+        [HttpPost]
+        [Route("GetEmployeeView")]
+        public List<EmployeeViewDto> GetEmployeeView(List<string> employeeList)
+        {
+            return _employeeViewService.GetEmployeeView(employeeList);
+        }
+        [HttpPost]
+        [Route("GetEmployeeByDeptView")]
+        public List<EmployeeViewDto> GetEmployeeByDeptView(List<string> DeptList, DateTime CheckDate)
+        {
+            return _employeeViewService.GetEmployeeByDeptView(DeptList, CheckDate);
+        }
+        [HttpPost]
+        [Route("GetEmployeeByManageView")]
+        public List<EmployeeViewDto> GetEmployeeByManageView(List<string> DeptList, DateTime CheckDate  ,bool IncludeDown)
+        {
+            return _employeeViewService.GetEmployeeByManageView(DeptList, CheckDate , IncludeDown);
         }
     }
     public class EmployeeInfo_GetPeopleByDept_Entry
