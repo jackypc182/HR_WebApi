dc862c3107e93bc6483598a917749faa47240e99
eeffew <eeffew@bbdc20ee-fbae-9e43-ab44-8db8742cc3c9>
2020-05-21 08:06:15 +0000

    swagger 說明檔案可相容於非iis express環境部屬
    git-svn-id: https://jb-vsts.jbjob.com.tw:8443/svn/customer/JBHR2/HR_WebApi/HR_WebApi@38945 bbdc20ee-fbae-9e43-ab44-8db8742cc3c9

--- Startup.cs
+++ Startup.cs
@@ -54,9 +54,14 @@ namespace HR_WebApi
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
-                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-                c.IncludeXmlComments(xmlPath);
+                // var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                // var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                // c.IncludeXmlComments(xmlPath);
+                
+                string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+                string commentsFileName = Assembly.GetExecutingAssembly().GetName().Name + ".XML";
+                string commentsFile = Path.Combine(baseDirectory, commentsFileName);
+                c.IncludeXmlComments(commentsFile);
             });
         }
 
