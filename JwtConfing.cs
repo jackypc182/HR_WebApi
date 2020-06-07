cef1b960f82ab29e45378c8f1da4aab057af03c9
stanley <stanley@bbdc20ee-fbae-9e43-ab44-8db8742cc3c9>
2020-06-07 14:28:12 +0000

    git-svn-id: https://jb-vsts.jbjob.com.tw:8443/svn/customer/JBHR2/HR_WebApi/HR_WebApi@39130 bbdc20ee-fbae-9e43-ab44-8db8742cc3c9

---
+++ JwtConfing.cs
@@ -0,0 +1,42 @@
+﻿using HR_WebApi.Helpers;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace HR_WebApi
+{
+    public class JwtConfing
+    {
+        public JwtConfing()
+        {
+
+        }
+
+        public static IServiceCollection Configure( IConfiguration Configuration , IServiceCollection services)
+        {
+            services.AddSingleton<JwtHelpers>();
+            services
+                .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.IncludeErrorDetails = true; // WWW-Authenticate 會顯示詳細錯誤原因
+
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        NameClaimType = Configuration.GetValue<string>("JwtSettings:NameClaim"),
+                        RoleClaimType = Configuration.GetValue<string>("JwtSettings:RoleClaim"),
+                        ValidateIssuer = true,
+                        ValidIssuer = Configuration.GetValue<string>("JwtSettings:Issuer"),
+                        ValidateAudience = false,
+                        //ValidAudience = Configuration.GetValue<string>("JwtSettings:Issuer"),
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = false, // 如果 JWT 包含 key 才需要驗證，一般都只有簽章而已
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration.GetValue<string>("JwtSettings:SignKey")))
+                    };
+                });
+            return services;
+        }
+    }
+}
