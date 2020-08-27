4630c07b2d33b67b637309e54cf40499b5c25f16
stanley <stanley@bbdc20ee-fbae-9e43-ab44-8db8742cc3c9>
2020-08-27 07:48:01 +0000

    add checkwithquery
    git-svn-id: https://jb-vsts.jbjob.com.tw:8443/svn/customer/JBHR2/HR_WebApi/HR_WebApi@39865 bbdc20ee-fbae-9e43-ab44-8db8742cc3c9

--- Controllers/WorkScheduleCheckController.cs
+++ Controllers/WorkScheduleCheckController.cs
@@ -3,31 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Mime;
 using System.Threading.Tasks;
+using JBHRIS.Api.Dto;
 using JBHRIS.Api.Dto.Attendance;
+using JBHRIS.Api.Dto.Attendance.Entry;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using NLog;
 
 namespace HR_Api_Demo.Controllers
 {
     /// <summary>
     /// 排班檢核服務
     /// </summary>
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class WorkScheduleCheckController : ControllerBase
     {
         private JBHRIS.Api.Service.Attendance.IWorkScheduleCheckService _workScheduleCheckService;
+        private ILogger _logger;
+
 
         /// <summary>
         /// 
         /// </summary>
         /// <param name="workScheduleCheckService"></param>
-        public WorkScheduleCheckController(JBHRIS.Api.Service.Attendance.IWorkScheduleCheckService workScheduleCheckService)
+        public WorkScheduleCheckController(JBHRIS.Api.Service.Attendance.IWorkScheduleCheckService workScheduleCheckService, NLog.ILogger logger)
         {
             _workScheduleCheckService = workScheduleCheckService;
+            _logger = logger;
         }
         /// <summary>
         /// 排班檢核
@@ -41,11 +47,51 @@ namespace HR_Api_Demo.Controllers
             //WorkScheduleCheckEntry workScheduleCheckEntry = new WorkScheduleCheckEntry();
             //workScheduleCheckEntry.CheckTypes.Add("CW7");
             //return _workScheduleCheckService.Check(JsonConvert.DeserializeObject<WorkScheduleCheckEntry>(workScheduleCheckEntry.ToString()));
-            WorkScheduleCheckResult result = DataCheck(workScheduleCheckEntry);
-            if (result.State)
+            //WorkScheduleCheckResult result = DataCheck(workScheduleCheckEntry);
+            //if (result.State)
+            _logger.Info("開始呼叫WorkScheduleCheck.Check");
                 return _workScheduleCheckService.Check(workScheduleCheckEntry);
-            else
-                return result;
+            //else
+            //    return result;
+        }
+        /// <summary>
+        /// 查詢現有班表資料，同時合併輸入的資料進行檢核班表是否有違規
+        /// </summary>
+        /// <param name="workScheduleCheckEntry">檢核條件</param>
+        /// <returns></returns>
+        [Route("CheckWithQuery")]
+        [HttpPost]
+        public ActionResult<WorkScheduleCheckResult> CheckWithQuery([FromBody] WorkScheduleCheckEntry workScheduleCheckEntry)// Check(dynamic workScheduleCheckEntry)//
+        {
+            _logger.Info("開始呼叫WorkScheduleCheck.CheckWithQuery");
+            return _workScheduleCheckService.CheckWithQuery(workScheduleCheckEntry);            
+        }
+        /// <summary>
+        /// 取得班別設定
+        /// </summary>
+        /// <returns></returns>
+        [Route("GetScheduleTypes")]
+        [HttpPost]
+        public string GetScheduleTypes()
+        {
+            _logger.Info("開始呼叫WorkScheduleCheck.GetScheduleTypes");
+            List<ScheduleTypeDto> result = new List<ScheduleTypeDto>();
+            result = _workScheduleCheckService.GetScheduleTypeList();
+            return JsonConvert.SerializeObject(result);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="workschedulecheckEntry"></param>
+        /// <returns></returns>
+        [Route("GetWorkschedules")]
+        [HttpPost]
+        public string GetWorkschedules([FromBody]WorkschedulecheckEntry workschedulecheckEntry  )
+        {
+            _logger.Info("開始呼叫WorkScheduleCheck.GetWorkschedules");
+            Dictionary<string, List<WorkScheduleDto>> workSchedulecheckDic = new Dictionary<string, List<WorkScheduleDto>>();
+            workSchedulecheckDic = _workScheduleCheckService.GetWorkScheduleList(workschedulecheckEntry);
+            return JsonConvert.SerializeObject(workSchedulecheckDic);
         }
 
         /// <summary>
@@ -73,7 +119,8 @@ namespace HR_Api_Demo.Controllers
                         ErrorMessage = string.Format("檢核日期：{0} 並無班表資料.", DT.ToString("yyyy-MM-dd")),
                     });
                 }
-                else if (!WSCD.ScheduleTypes.Where(p => p.Code == WSCD.WorkSchedules.Where(p => p.AttendanceDate == DT).First().ScheduleType).Any())
+                else 
+                if (!WSCD.ScheduleTypes.Where(p => p.Code == WSCD.WorkSchedules.Where(p => p.AttendanceDate == DT).First().ScheduleType).Any())
                 {
                     result.State = false;
                     result.workScheduleIssues.Add(new WorkScheduleIssueDto
