c74049b7dbb55684c0c3e16e1e971b0f9977327c
stanley <stanley@bbdc20ee-fbae-9e43-ab44-8db8742cc3c9>
2020-08-27 08:10:42 +0000

    git-svn-id: https://jb-vsts.jbjob.com.tw:8443/svn/customer/JBHR2/HR_WebApi/HR_WebApi@39870 bbdc20ee-fbae-9e43-ab44-8db8742cc3c9

---
+++ Controllers/StatusController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JBHRIS.Api.Dto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace HR_WebApi.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class StatusController : ControllerBase
+    {
+        private IConfiguration _configuration;
+
+        public StatusController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        [HttpGet]
+        public string ServiceStatus()
+        {
+            return "服務執行中.....";
+        }
+    }
+}
