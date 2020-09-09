9cd64d8c4854e5c9f4c362f6bae6ace94cc44d6c
stanley <stanley@bbdc20ee-fbae-9e43-ab44-8db8742cc3c9>
2020-09-09 07:06:36 +0000

    add system controller
    git-svn-id: https://jb-vsts.jbjob.com.tw:8443/svn/customer/JBHR2/HR_WebApi/HR_WebApi@39973 bbdc20ee-fbae-9e43-ab44-8db8742cc3c9

---
+++ Controllers/_System/RoleController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HR_WebApi.Controllers._System
+{
+    /// <summary>
+    /// 權限控制器
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoleController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "";
+        }
+    }
+}
