fea98e5eb2929a426bad01b4bfe2eaeb22cbe8ea
stanley <stanley@bbdc20ee-fbae-9e43-ab44-8db8742cc3c9>
2020-09-21 15:48:40 +0000

    修改-setting對應nameclaim
    git-svn-id: https://jb-vsts.jbjob.com.tw:8443/svn/customer/JBHR2/HR_WebApi/HR_WebApi@40072 bbdc20ee-fbae-9e43-ab44-8db8742cc3c9

--- Helpers/JwtHelpers.cs
+++ Helpers/JwtHelpers.cs
@@ -19,9 +19,9 @@ namespace HR_WebApi.Helpers
 
             // 在 RFC 7519 規格中(Section#4)，總共定義了 7 個預設的 Claims，我們應該只用的到兩種！
             //claims.Add(new Claim(JwtRegisteredClaimNames.Iss, issuer));
-            claims.Add(new Claim(JwtRegisteredClaimNames.Sub, userName)); // User.Identity.Name
-            claims.Add(new Claim(ClaimTypes.NameIdentifier, userName)); // User.Identity.Name
-            
+            //claims.Add(new Claim(JwtRegisteredClaimNames.Sub, userName)); // User.Identity.Name
+                                                                          //claims.Add(new Claim(ClaimTypes.NameIdentifier, userName)); // User.Identity.Name
+
             //claims.Add(new Claim(JwtRegisteredClaimNames.Aud, "The Audience"));
             //claims.Add(new Claim(JwtRegisteredClaimNames.Exp, DateTimeOffset.UtcNow.AddMinutes(30).ToUnixTimeSeconds().ToString())); // 必須為數字
             //claims.Add(new Claim(JwtRegisteredClaimNames.Nbf, DateTimeOffset.UtcNow.ToUnixTimeSeconds().ToString())); // 必須為數字
@@ -45,6 +45,7 @@ namespace HR_WebApi.Helpers
             //claims.Add(new Claim("roles", "Users"));
 
             var userClaimsIdentity = new ClaimsIdentity(claims);
+            var tt = userClaimsIdentity.NameClaimType;
 
             // 建立一組對稱式加密的金鑰，主要用於 JWT 簽章之用
             var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(signKey));
